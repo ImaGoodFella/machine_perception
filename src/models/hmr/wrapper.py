7ebb03b5698d2b1e1ae9ee0a805f6b0960e580db
ImaGoodFella <steigerten@gmail.com>
2023-06-13 02:35:25 -0700

    Added larger resnet + ELU

--- src/models/hmr/wrapper.py
+++ src/models/hmr/wrapper.py
@@ -9,7 +9,8 @@ class Wrapper(GenericWrapper):
     def __init__(self, args):
         super().__init__(args)
         self.model = HMR(
-            backbone='resnext101_32x8d',
+            #backbone='resnext101_32x8d',
+            backbone="resnet152",
             #backbone="resnet50",
             #backbone="resnet18",
             focal_length=args.focal_length,
